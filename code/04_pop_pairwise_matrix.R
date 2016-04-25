ae6bfa614b744c05ecbd4483465ea240c4d2a490
Fernando Cagua <efcaguab@gmail.com>
2016-04-26 10:40:36 +1200

    save pairwise matrix

--- code/04_pop_pairwise_matrix.R
+++ code/04_pop_pairwise_matrix.R
@@ -5,5 +5,7 @@ library(dplyr)
 env_pca <- readRDS("data/processed/env_pca.rds")
 dist_matrix <- readRDS("data/processed/distance_matrix.rds")
 
-dist_matrix %>% as.data.frame.table() %>%
+pairwise_matrix <- dist_matrix %>% as.data.frame.table() %>%
 	dplyr::rename(sample1 = Var1, sample2 = Var2, geo = Freq)
+
+write.csv(pairwise_matrix, file = "data/processed/pairwise_matrix.csv", row.names = F)
