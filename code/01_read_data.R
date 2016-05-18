5085b6f1a436f44ef16f4d6e2eab13b967af8bf1
Fernando Cagua <efcaguab@gmail.com>
2016-05-18 19:34:56 +1200

    add data per species instead of location.csv

--- code/01_read_data.R
+++ code/01_read_data.R
@@ -1,6 +1,6 @@
 # get data from BioOracle for the GPS fixes in `locations.csv` outputs
-# 'locations.rds' with details about the study sites and `env_data.rds` with the
-# raw environmental data from BioOracle. Rows are in same order as in env_data.rds
+# 'locations.rds' with contains a list of the study sites per fish and `env_data.rds` with the
+# raw environmental data from BioOracle per fish. Rows are in same order as in env_data.rds
 
 library(magrittr)
 library(ggplot2)
@@ -21,29 +21,41 @@ raster_names <- list.files(folder) %>%
 env_data <- SDMTools::asc2dataframe(raster, raster_names) %>% 
 	dplyr::tbl_df()
 
-# read locations
+# read species
+species <- "./data/raw/species" %>%
+	list.files(full.names = T) %>%
+	lapply(read.csv)
+
+nam <- "./data/raw/species" %>%
+	list.files(full.names = F) %>%
+	stringr::str_sub(end = -5)
+
+names(species) <- nam
 locations <- read.csv("data/raw/locations.csv")
 
-# subset data
-env_data %<>%
-	dplyr::filter(round(y) %in% round(locations$latitude),
-								round(x) %in% round(locations$longitude))
+env_data_filtered <- lapply(species, function(x){
+	env_data %>%
+		dplyr::filter(round(y) %in% round(x$latitude),
+									round(x) %in% round(x$longitude))
+})
 
-data <- locations %>%
-	plyr::ddply("population", function(x){
-		i <- geosphere::distHaversine(c(x$longitude, x$latitude), 
-																	as.matrix(env_data[, c("x", "y")])) %>%
-			which.min()
-		env_data %>%
-			dplyr::slice(i) %>%
-			cbind(x, .)
-	})
+for(i in 1:length(species)){
+	env_data_filtered[[i]] <- species[[i]] %>%
+		plyr::ddply("population", function(x){
+			j <- geosphere::distHaversine(c(x$longitude, x$latitude), 
+																		as.matrix(env_data_filtered[[i]][, c("x", "y")])) %>%
+				which.min()
+			env_data_filtered[[i]] %>%
+				dplyr::slice(j) %>%
+				cbind(x, .)
+		})
+}
 
-locations <- data %>%
-	dplyr::select(name:x)
+locations <- env_data_filtered %>%
+	lapply(dplyr::select, name:x)
 
-data %<>% 
-	dplyr::select(calcite:sstrange)
+data <- env_data_filtered %>% 
+	lapply(dplyr::select, calcite:sstrange)
 
 saveRDS(locations, file = "data/processed/locations.rds", ascii = T, compress = F)
 saveRDS(data, file = "data/processed/env_data.rds", ascii = T, compress = F)
